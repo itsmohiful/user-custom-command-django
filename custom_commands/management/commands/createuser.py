8dd5989475fb9e522be6723aecc8136db3750a1f
Soyad Mohiful Islam <mohiful.islam.ofc@gmail.com>
2022-07-28 13:39:40 +0600

    add positiona arguments and plug arguments

--- custom_commands/management/commands/createuser.py
+++ custom_commands/management/commands/createuser.py
@@ -10,14 +10,22 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('quantity', type=int, help='number of user you want to create.')
+        parser.add_argument('-a', '--admin', action='store_true', help='Define admin account')
 
 
     def handle(self, *args, **kwargs):
         quantity = kwargs['quantity']
+        admin = kwargs['admin']
 
         for i in range(quantity):
             username = get_random_string(15)
             password = get_random_string(8)
 
-            User.objects.create_user(username=username, email='', password=password)
+            if admin:
+                User.objects.create_superuser(username=username, email='', password=password)
+
+                self.stdout.write("Super User '%s (%s)' has been created!" %(username, password))
+            else:
+                User.objects.create_user(username=username, email='', password=password)
             
+                self.stdout.write("User '%s (%s)' has been created!" %(username, password))
